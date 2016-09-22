aaa262ef44fd325caea5d2c915f5bc6aea42cbe0
polypmer <exorable.ludos@gmail.com>
2016-09-21 23:43:16 -0400

    cryptopals woooah this looks fun

---
+++ cryptopals/set_one.go
@@ -0,0 +1,12 @@
+package main
+
+func c1() {
+	given := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	expected := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+	result := // something rather
+	fmt.Printf("Challenge 1: %t", result == expected)
+}
+
+func main() {
+	c1()
+}
