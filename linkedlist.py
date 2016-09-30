24b08b3ae4c6fb0d392579c7b6ae011e6171c175
polypmer <exorable.ludos@gmail.com>
2016-09-30 15:26:43 -0400

    Added linkedlist parent

--- linkedlist.py
+++ linkedlist.py
@@ -1,12 +1,16 @@
 class Node(object):
+    """Node is the basic object in a linked list"""
     value = ""
-    reference = []
+    reference = [] # Why is this an Empty node?
+    parent = []
 
     def __init__(self, v):
+        """constructor"""
         self.value = v
         # Add empty Node value?
 
     def __str__(self):
+        """print string"""
         represent = self.value # TODO print reference
         return represent
         
@@ -14,21 +18,26 @@ class Node(object):
         """add"""
         if not self.reference:
             self.reference = Node(v)
+            self.reference.parent = self
         else:
             self.reference.add(v)
 
     def rem(self):
-        """remove"""
+        """remove next node"""
         new_next = self.reference.reference
         self.reference = new_next
+        new_next.parent = self
 
     def ins(self, v):
         """insert"""
         old_reference = self.reference
         self.reference = Node(v)
+        self.reference.parent = self
         self.reference.reference = old_reference
+        old_reference.parent = self.reference
         
     def list_nodes(self):
+        """list all nodes"""
         print(self)
         nxt = self.reference
         print(nxt)
@@ -38,6 +47,7 @@ class Node(object):
             else:
                 print(nxt.reference)
                 nxt = nxt.reference
+
 
 
 if __name__ == "__main__":
@@ -50,4 +60,5 @@ if __name__ == "__main__":
     print(n.list_nodes())
     n.ins("The New Second")
     print(n.list_nodes())
+    print(n.reference.parent)
     
