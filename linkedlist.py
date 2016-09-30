146a5a4f6137d3e0448abfc89b79302e6046e14b
polypmer <exorable.ludos@gmail.com>
2016-09-30 15:21:41 -0400

    Added insert linked lists

--- linkedlist.py
+++ linkedlist.py
@@ -11,14 +11,22 @@ class Node(object):
         return represent
         
     def add(self, v):
+        """add"""
         if not self.reference:
             self.reference = Node(v)
         else:
             self.reference.add(v)
 
     def rem(self):
+        """remove"""
         new_next = self.reference.reference
         self.reference = new_next
+
+    def ins(self, v):
+        """insert"""
+        old_reference = self.reference
+        self.reference = Node(v)
+        self.reference.reference = old_reference
         
     def list_nodes(self):
         print(self)
@@ -39,5 +47,7 @@ if __name__ == "__main__":
     n.add("The Fourth")
     print(n.list_nodes())
     n.rem()
+    print(n.list_nodes())
+    n.ins("The New Second")
     print(n.list_nodes())
     
