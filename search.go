948c3841e78af2ab001079c171371187e654f6d1
polypmer <exorable.ludos@gmail.com>
2016-10-21 15:20:25 -0400

    Computers dead

--- search.go
+++ search.go
@@ -3,24 +3,32 @@ package main
 import "fmt"
 
 func main() {
+	x := []int{2, 3, 4, 6, 8, 7, 11, 11, 11, 45, 54, 65}
+	fmt.Println(search(4, x))
 }
 
 func search(target int, sorted []int) int {
 	// Find the indexes
-	L := 0
-	H := len(sorted) - 1
-	M := len(sorted) / 2
-	if target > H {
+	low := 0
+	high := len(sorted) - 1
+	mid := len(sorted) / 2
+	if target > high {
 		fmt.Println("wtf")
 		return -1
 	}
 	// Find the value
 	middle := sorted[len(sorted)/2]
+
 	if target == middle {
-		return M
+		return mid
 	} else if target > middle {
-		return
+		// should this be [middle+1:]??
+		newList := sorted[middle:high]
+		return search(target, newList)
 	} else if target < middle {
-		return
+		newList := sorted[low:middle]
+		fmt.Println(newList)
+		return search(target, newList)
 	}
+	return mid
 }
