895e49d3cdcb7eb42817eb7469c179959f1d64cd
polypmer <exorable.ludos@gmail.com>
2016-10-22 14:24:25 -0400

    Racket makes things slightly easier

--- sort.scm
+++ sort.scm
@@ -1,49 +1,45 @@
+#lang racket
 (newline)
 ;; I like newline
 
 
 ;; merge sort
-" I need to define a split and a merge "
-" and then merge_sort"
-
 (define (split x) ;; split is a function of one arg
   "Split will split a list into two lists"
-  (define middle (/ (length x) 2))
-  (define (before? x) (< x middle)) ;; this deosn't make sense
-  ;; first half:
-  (filter ;; returns a list of elements for which item returns true
-   before? x)
-  ;;(list 0 middle) ;; create new lists...
+  ;; TODO: Doesn't work with odd lists
+  (define mididx
+    (if
+     (odd? (length x))
+     ("Doesn't work")  ;;((/ (- (length x) 1) 2))
+     (/ (length x) 2))
+    )
+  ;; Get first and second half of list
+  (list (take x  mididx) (drop x mididx))
   )
+
+
 
 (define (merge a b c)
+  "Merge merges two already sorted list"
+  (append c (list
   (cond
-   ((< (car a) (car b)) (append c (list (car a))))
-   ((> (car a) (car b)) (append c (list (car b))))
+   ((< (car a) (car b)) (car a))
+   ((< (car b) (car a)) (car b));;(append c (list (car a)))
    )
-  (display c)
-  (if
-   (not (or (null? b) (null? a)))
-   (merge a b c)
-   (display "ye"))
-  )
-
-(define (t a b c)
-  (if
-   (< (car a) (car b))
-   (append c (list (car a)))
-   )
-
-;;s  (if
-  ;;(display (> (car a) (car b)))
+  ))
+  (define x "a")
+  (define y "b")
+  (c)
   )
 
 
-;;(display (split (list 1 5 4 2)))
+;
+(display (split '(1 5 4 2)))
 ;;(display (merge (list 1 2 3) (list 6 5 4) '()))
-(define l '())
-(display (t (list 1 2 3) (list 6 5 4) (t (list 1 2 3) (list 6 5 4) l)))
+;; I can do (list n n n) or literal: `(n n n)
 
+;;(display (merge `(1 2 3) `(6 5 4)
+(merge (list 1 2 3) (list 6 5 4) '())
 
 
 
