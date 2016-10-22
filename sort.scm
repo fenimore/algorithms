18b0a554c0ab0e08b1417251f1f114abf9409302
polypmer <exorable.ludos@gmail.com>
2016-10-22 15:25:16 -0400

    Merge is working in racket

--- sort.scm
+++ sort.scm
@@ -1,16 +1,17 @@
 #lang racket
 (newline)
-;; I like newline
 
 
 ;; merge sort
+
+;; Split
 (define (split x) ;; split is a function of one arg
   "Split will split a list into two lists"
   ;; TODO: Doesn't work with odd lists
   (define mididx
     (if
      (odd? (length x))
-     ("Doesn't work")  ;;((/ (- (length x) 1) 2))
+     ("Doesn't work on odd lists")  ;;((/ (- (length x) 1) 2))
      (/ (length x) 2))
     )
   ;; Get first and second half of list
@@ -20,57 +21,59 @@
 
 
 (define (merge a b c)
-  "Merge merges two already sorted list"
-  (define first-a (car a)) ;; first a
-  (define first-b (car b)) ;; first b
+   "Merge merges two already sorted list"
+   ;;(define first-a (car a)) ;; first a
+   ;;(define first-b (car b)) ;; first b
+   ;; Add the lowest to c list
+   "(define z
+     (append c
+             (cond
+              ((> (car a) (car b))(car b))
+              ((< (car a) (car b))(car a))
+              )))"
 
-  ;; Add the lowest to c list
-  (define z
-    (append c
-            ((cond
-              ((> first-a first-b)(first-b))
-              ((< first-a first-b)(first-a))
-              )
-             )
-            )
-    )
-  ;; Pop the item off such a list
-  ;; x is the popped list
-  (define x
-    (
-     (cond
-      ((> first-a first-b)(drop b 0))
-      ((< first-a first-b)(drop a 0))
+   (let* (
+          [z
+           (append c
+                   (cond
+                    ((> (car a) (car b))(car b))
+                    ((< (car a) (car b))(car a))
+                    )
+                   )]
+          ;; Pop the item off such a list
+          ;; x is the popped list
+          [x
+           (cond
+            ((> (car a) (car b))(drop b 1))
+            ((< (car a) (car b))(drop a 1))
+            )]
+          ;; The list not popped
+          [y
+           (cond
+            ((> (car a) (car b)) a)
+            ((< (car a) (car b)) b)
+            )]
+          )
+     ;; Call merge if the lists are not 0
+     ;; pass in x for the popped list
+     ;; Emptiness is true if the poped list is empy
+     ;;(display '(x y z))
+     ;;(display z)
+     (if
+      (null? x)
+      (append (list z) y)
+      (merge x y (list z))
       )
+     ;;(list x y z)
      )
-    )
-
-  ;; The list not popped
-  (define y
-    (
-     (cond
-      ((> first-a first-b)(a))
-      ((< first-a first-b)(b))
-      )
-     )
-    )
-
-  ;; Call merge if the lists are not 0
-  ;; pass in x for the popped list
-  ;; Emptiness is true if the poped list is empy
-  (if
-   (null? x)
-   (append z y)
-   (merge x y z)
    )
-  )
 
 
 
 
 ;;(display (split '(1 5 4 2)))
 ;;(merge (list 1 2 3) (list 6 5 4) '())
-(merge ('(1 2 3)) ('(6 5 4)) '())
+(merge '(1 5 7) '(4 6 8) '())
 
 
 
