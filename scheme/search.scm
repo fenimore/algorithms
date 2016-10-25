57ed8e6054c1dee6fc750667e9d31940c705b36a
polypmer <exorable.ludos@gmail.com>
2016-10-25 09:04:20 -0400

    Binary search in scheme now works

--- scheme/search.scm
+++ scheme/search.scm
@@ -2,30 +2,34 @@
 
 (define sorted-list '(2 4 5 6 8 14 15 17))
 
-;; Split
-(define (split x) ;; split is a function of one arg
-  "Split will split a list into two lists"
-  (define mididx
-    (if
-     (odd? (length x))
-     (/ (- (length x) 1) 2)
-     (/ (length x) 2)))
-  ;; Get first and second half of list
-  (list (take x  mididx) (drop x mididx)))
 
-(define (binary-search lst target)
+(define (binary-search lst target high low)
   "Binary Search for index of target number"
+  ;; Always takes length of list for high and
+  ;; 0 for low
   ;; TODO: If number outside of range, fail
   (define mididx
     (if
-     (odd? (length lst))
-     (/ (- (length lst) 1) 2)
-     (/ (length lst) 2)))
+     (odd? (+ low high))
+     (/ (- (+ low high) 1) 2)
+     (/ (+ low high) 2)))
+
   (cond
+   ((> target (list-ref lst (- high 1))) "Not Found")
    ((= (list-ref lst mididx) target) mididx)
-   ((= (length lst) 1) "Item Not Found")
-   ((< target (list-ref lst mididx)) (binary-search (car (split lst)) target))
-   ((> target (list-ref lst mididx)) (binary-search (cdr (split lst)) target))))
+   ((< target (list-ref lst mididx)) (binary-search lst target mididx low))
+   ((> target (list-ref lst mididx)) (binary-search lst target high mididx))
+   )
+  )
 
+(display "Search For 5 ")
+(binary-search sorted-list 5 (length sorted-list) 0)
 
-(binary-search sorted-list 15)
+(display "Search For 15 ")
+(binary-search sorted-list 15 (length sorted-list) 0)
+
+(display "Search For 33 ")
+(binary-search sorted-list 33 (length sorted-list) 0)
+
+(display "Search For 3 ")
+(binary-search sorted-list 3 (length sorted-list) 0)
